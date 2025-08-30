d6e04996013eae7ae04ce34206a9266ea0fc0a8f
Pablo Muris <PabloMuris@protonmail.com>
2025-08-30 15:47:14 -0300

    Add responsive layout and navigation buttons
    - Implemented a responsive layout in global.js that adjusts the grid structure based on the viewport width for mobile, intermediate, and desktop views.
    - Added functionality to show/hide navigation paragraph elements based on the screen size.
    - Created a new navigation section in navigation_section.html with buttons for returning and adding, enhancing user interaction.

--- koltagri/landplots/urls.py
+++ koltagri/landplots/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path,re_path
-from .views import CultivatesView
+from .views import CultivatesView,CultivatesDetailView
 urlpatterns = [
-    path('',CultivatesView.as_view(),name='cultivates')
+    path('',CultivatesView.as_view(),name='cultivates'),
+    path('detalhes',CultivatesDetailView.as_view(),name='cultivates_detail'),
+    path('site')
 ]
--- koltagri/landplots/views.py
+++ koltagri/landplots/views.py
@@ -7,3 +7,6 @@ from django.views.generic import TemplateView
 
 class CultivatesView(TemplateView):
     template_name = 'cultivos.html'
+
+class CultivatesDetailView(TemplateView):
+    template_name = 'cultivate_detail.html'
