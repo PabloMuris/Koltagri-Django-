1c43b10d81cfcb9a38439f6801850b06f725da89
Pablo Muris <PabloMuris@protonmail.com>
2025-08-30 15:47:45 -0300

    Remove incomplete URL path entry from landplots URL configuration

--- koltagri/landplots/urls.py
+++ koltagri/landplots/urls.py
@@ -3,5 +3,4 @@ from .views import CultivatesView,CultivatesDetailView
 urlpatterns = [
     path('',CultivatesView.as_view(),name='cultivates'),
     path('detalhes',CultivatesDetailView.as_view(),name='cultivates_detail'),
-    path('site')
 ]
