d2fa133c0539e20444369d4b388c68c998ea629a
Pablo Muris <PabloMuris@protonmail.com>
2025-10-20 14:23:20 -0300

    Add sidebar animation styles and update template structure: create base.css for menu animations, link it in base.html, and adjust index.html for script block.

--- static/js/base.js
+++ static/js/base.js
@@ -3,20 +3,22 @@ const asideList = document.getElementsByTagName("aside");
 const aside = asideList[0];
 
 
-function ChangeSideBarWidth(){
+function retractSideBarWidth(){
 const screenWidth = window.innerWidth;
 
-    // Define os pontos de corte (breakpoints) e as larguras desejadas
-    if (screenWidth > 769 && screenWidth<1024) {
-        // Exemplo: Telas pequenas (celulares), aside tem 100% da largura
-        aside.style.width = '76px';
-        aside.style.position = 'sticky9'; // Volta a ser um elemento normal no fluxo
-    }
-    else {
-        if (screenWidth>=1024){
-            
-        }
-    }
+
+if (screenWidth>769 && screenWidth<=1024){
+
+}
+if (screenWidth>1024){
+    
+}
+}
+
+
+
+function expandSideBarWidth(){
+    return 'mmm'
 }
 
 
