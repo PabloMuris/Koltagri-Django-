e42a3b4acc01a419da0656c1a74346fcab7f5515
Pablo Muris <PabloMuris@protonmail.com>
2025-11-14 13:59:20 -0300

    refactor: Update filter and search components for improved layout and styling

--- koltagri/business/urls.py
+++ koltagri/business/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
    path("",BusinessDashboardView.as_view(),name="business_board"),
    path("gastos/",StatisticsView.as_view(),name="statistics"),
    path("insumos/",SuppliesView.as_view(),name="supplies"),
-   path("insumo",SuppliesDetailView.as_view(),name="supplies_detail")
+   path("insumos/insumo",SuppliesDetailView.as_view(),name="supplies_detail")
 ]
